c7db75e000451b1c55c71024a7662a5ac26faa5a
andela-vdugeri <verem.dugeri@andela.co>
2015-09-09 08:18:45 +0100

    Implement the Rankwords class and the test

--- tests/RankWordsTest.php
+++ tests/RankWordsTest.php
@@ -9,11 +9,29 @@
 namespace Dictionary\Test;
 
 
+use Dictionary\Dictionary;
+use Dictionary\ModifyDictionary;
+use Dictionary\RankWords;
+
 class RankWordsTest extends \PHPUnit_Framework_TestCase {
 
 
 	public function testTrue()
 	{
-		$this->assertEquals(true, true);
+		$dictionary = Dictionary::getDictionary();
+		$modifier = new ModifyDictionary();
+
+		$this->assertEquals(0, count($dictionary));
+		$array = $modifier->createEntry("tight",
+				"When someone shows approval",
+				"Andrei: Prosper, have you completed the curriculum.\nProsper: Yes.\nAndrei: Tight, Tight, Tight!!!.");
+		$this->assertEquals(2, count($array));
+		$this->assertEquals('When someone shows approval', $array['meaning']);
+
+		$ranked = RankWords::rankWords($array['sample-sentence']);
+  	$this->assertEquals(2, $ranked['Prosper']);
+		$this->assertEquals(2, $ranked['Andrei']);
+		$this->assertEquals(3, $ranked['Tight']);
+		$this->assertNotEquals(3, $ranked['Prosper']);
 	}
 }
